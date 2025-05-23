9d667bc5d940fc6cf35b17857f284acee491eb6a
Nia (Diem Nguyen) <diemn@joblogic.com>
2025-05-23 15:47:32 +0700

    feat: handle pagingation

--- controller/movie_controller.py
+++ controller/movie_controller.py
@@ -2,14 +2,24 @@ from database.database import movies_collection
 from model.movie_model import Movie
 from bson import ObjectId
 from service.conversion_service import convert_objectid_and_decimal
+from typing import Dict, Any
 
 class MovieController:
     
     @staticmethod
-    def get_movies():
-            results = movies_collection.find()
-            movies = Movie.to_list([convert_objectid_and_decimal(result) for result in results])
-            return movies
+    def get_movies(page: int = 1, limit: int = 10) -> Dict[str, Any]:
+            skip = (page - 1) * limit
+            total = movies_collection.count_documents({})
+            cursor = movies_collection.find().skip(skip).limit(limit)
+            movies = [Movie.to_item(movie) for movie in cursor]
+            
+            return {
+                "page": page,
+                "limit": limit,
+                "total_items": total,
+                "total_pages": (total + limit - 1) // limit,
+                "data": [m.model_dump() for m in movies]
+            }
 
     @staticmethod
     def get_movie(_id: str):
--- route/movie_route.py
+++ route/movie_route.py
@@ -1,11 +1,12 @@
 from fastapi import APIRouter, HTTPException
 from controller.movie_controller import MovieController
+from fastapi import APIRouter, Query
 
 router = APIRouter()
 
 @router.get("/movie/")
-async def get_all_movies():
-    return MovieController.get_movies()
+async def get_all_movies(page: int = Query(1, ge=1), limit: int = Query(10, le=100)):
+    return MovieController.get_movies(page, limit)
 
 @router.get("/movie/{movie_id}")
 async def get_movie_detail(movie_id: str):
